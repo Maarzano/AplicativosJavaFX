3d565a344b677530adbbe0c02ab90d9a635cd82e
Maarzano <arthuramarzano10@gmail.com>
2025-02-12 21:40:05 -0300

    Não teerminei de mexer

--- demo/src/main/java/org/example/demo/HelloApplication.java
+++ demo/src/main/java/org/example/demo/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 520, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 420, 240);
         stage.setTitle("Fuck!");
         stage.setScene(scene);
         stage.show();
--- demo/src/main/java/org/example/demo/HelloController.java
+++ demo/src/main/java/org/example/demo/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Acho que vc é boyola");
     }
 }
