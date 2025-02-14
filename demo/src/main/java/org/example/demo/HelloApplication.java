9b1b90d8da32657bc69c6f5934db1c4f06b01fc8
Maarzano <arthuramarzano10@gmail.com>
2025-02-13 21:21:51 -0300

    testando coisas com JavaFX

--- demo/src/main/java/org/example/demo/HelloApplication.java
+++ demo/src/main/java/org/example/demo/HelloApplication.java
@@ -3,6 +3,8 @@ package org.example.demo;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,8 +13,11 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 420, 240);
-        stage.setTitle("Fuck!");
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Image icon = new Image("D:\\Maarzano\\Repo\\RepoTeste\\demo\\src\\Images\\3135768.png");
+        stage.getIcons().add(icon);
+        stage.setTitle("Controle de Biblioteca");
+        scene.getStylesheets().add(HelloApplication.class.getResource("style.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
     }
