3e7f0bd0aaf733f0cabc0fa1b7d8e44a491fd397
Marzano <arthuramarzano10@gmail.com>
2025-02-19 10:46:46 -0300

    colocando icon na janela

--- AppConversãoTemperatura/conversortemperatura/src/main/java/com/conversortemperatura/App.java
+++ AppConversãoTemperatura/conversortemperatura/src/main/java/com/conversortemperatura/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,6 +16,8 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("primary"), 350, 280);
+        Image iconImage = new Image(getClass().getResource("/com/conversortemperatura/imagens/png-clipart-round-black-logo-computer-icons-temperature-measurement-temperature-save-miscell.png").toExternalForm());
+        stage.getIcons().add(iconImage);
         scene.getStylesheets().add(getClass().getResource("/com/conversortemperatura/styles.css").toExternalForm());
         stage.setTitle("Conversor de Temperatura");
         stage.setScene(scene);
