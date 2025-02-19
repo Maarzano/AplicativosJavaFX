14296746f1703b6573f2e4389e350dd46a6dc79e
Marzano <arthuramarzano10@gmail.com>
2025-02-19 10:51:21 -0300

    retirando aviso

--- AppConversãoTemperatura/conversortemperatura/src/main/java/module-info.java
+++ AppConversãoTemperatura/conversortemperatura/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.conversortemperatura {
     requires javafx.controls;
     requires javafx.fxml;
+    requires transitive javafx.graphics;
 
     opens com.conversortemperatura to javafx.fxml;
     exports com.conversortemperatura;
