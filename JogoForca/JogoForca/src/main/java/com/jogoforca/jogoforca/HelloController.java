fd5b4d508c6f6f06bd4fd7e698b1a17ca2185aea
Arthur-Alves <alves2araujo@gmail.com>
2025-02-19 20:23:56 -0300

    primeiros passos no projeto forca

--- JogoForca/JogoForca/src/main/java/com/jogoforca/jogoforca/HelloController.java
+++ JogoForca/JogoForca/src/main/java/com/jogoforca/jogoforca/HelloController.java
@@ -3,6 +3,8 @@ package com.jogoforca.jogoforca;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+import java.util.Scanner;
+
 public class HelloController {
     @FXML
     private Label welcomeText;
@@ -10,5 +12,61 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    Scanner Scan = new Scanner(System.in);
+    int tentativaF = 6;
+    char[] Palavra = {'c', 'a', 'c', 'h', 'o', 'r', 'r', 'o'};
+    char[] LetrasTen = {'_', '_', '_', '_', '_', '_', '_', '_'};
+
+        System.out.println("Bem-vindo ao jogo da forca!");
+        System.out.println("Você tem 6 tentativas para adivinhar a palavra.");
+
+        while (tentativaF > 0) {
+
+        System.out.print("Palavra: ");
+        int i = 0;
+        while (i < LetrasTen.length) {
+            System.out.print(LetrasTen[i] + " ");
+            i++;
+        }
+        System.out.println();
+
+        System.out.println("Chute uma letra: ");
+        char chute = Scan.next().toLowerCase().charAt(0);
+
+        boolean acertou = false;
+        i = 0;
+        while (i < Palavra.length) {
+            if (Palavra[i] == chute) {
+                LetrasTen[i] = chute;
+                acertou = true;
+            }
+            i++;
+        }
+
+        if (!acertou) {
+            tentativaF--;
+            System.out.println("Errou! Você tem " + tentativaF + " tentativas restantes.");
+        }
+
+        boolean ganhou = true;
+        i = 0;
+        while (i < LetrasTen.length) {
+            if (LetrasTen[i] == '_') {
+                ganhou = false;
+                break;
+            }
+            i++;
+        }
+
+        if (ganhou) {
+            System.out.println("Parabéns, você adivinhou a palavra!");
+            break;
+        }
+    }
+
+        if (tentativaF == 0) {
+        System.out.println("Você perdeu! A palavra era: " + new String(Palavra));
     }
 }
