23e8b3e2b0ce9219fdd9cf2bcbc751c52c0265ac
Marzano <arthuramarzano10@gmail.com>
2025-02-19 12:03:05 -0300

    fazendo essa budega funcionar

--- SistemaLogin/sistemalogin/src/main/java/com/sistema/login/LoginApp.java
+++ SistemaLogin/sistemalogin/src/main/java/com/sistema/login/LoginApp.java
@@ -9,9 +9,9 @@ import javafx.stage.Stage;
 public class LoginApp extends Application {
 
     @Override
-    public void start(@SuppressWarnings("exports") Stage primaryStage) throws Exception {
-        // Carrega o arquivo FXML
-        Parent root = FXMLLoader.load(getClass().getResource("Tela.fxml"));
+    public void start(Stage primaryStage) throws Exception {
+        // Carrega o arquivo FXML corretamente
+        Parent root = FXMLLoader.load(getClass().getResource("/com/sistema/login/Tela.fxml"));
         
         Scene scene = new Scene(root, 300, 200);
         primaryStage.setTitle("Tela de Login");
