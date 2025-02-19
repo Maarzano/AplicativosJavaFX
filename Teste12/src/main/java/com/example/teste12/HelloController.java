c620dff37a54fcaa9ce2e89cb63e69c29e07b513
Arthur-Alves <alves2araujo@gmail.com>
2025-02-18 21:40:43 -0300

    Separando classe

--- Teste12/src/main/java/com/example/teste12/HelloController.java
+++ Teste12/src/main/java/com/example/teste12/HelloController.java
@@ -31,13 +31,12 @@ public class HelloController {
                     nota2 = Double.parseDouble(nota2Field.getText());
                     nota3 = Double.parseDouble(nota3Field.getText());
 
+                    Media media = new Media(nota1, nota2, nota3);
 
-                    double media = (nota1 + nota2 + nota3) / 3;
-
-                    if (media >= 6.0) {
-                        resultadoLabel.setText("Aprovado! Média: " + String.format("%.1f" , media));
+                    if (media.calculadorDeMedia() >= 6.0) {
+                        resultadoLabel.setText("Aprovado! Média: " + String.format("%.1f" , media.calculadorDeMedia()));
                     } else {
-                        resultadoLabel.setText("Reprovado! Média: " + String.format("%.1f" , media));
+                        resultadoLabel.setText("Reprovado! Média: " + String.format("%.1f" , media.calculadorDeMedia()));
                     }
                 } else {
                     resultadoLabel.setText("Não pode nota maior que 10");
