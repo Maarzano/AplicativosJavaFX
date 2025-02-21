5cb4ef6c804f6a83347b79122fb731447b19bce9
Arthur-Alves <alves2araujo@gmail.com>
2025-02-20 21:09:49 -0300

    finalização BETA jogo da forca

--- JogoForca/JogoForca/src/main/java/com/jogoforca/jogoforca/HelloApplication.java
+++ JogoForca/JogoForca/src/main/java/com/jogoforca/jogoforca/HelloApplication.java
@@ -3,6 +3,7 @@ package com.jogoforca.jogoforca;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +12,9 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 300);
+        Image iconImage = new Image(getClass().getResource("/com/jogoforca/jogoforca/imagens/252192.png").toExternalForm());
+        stage.getIcons().add(iconImage);
+        Scene scene = new Scene(fxmlLoader.load(), 320, 450);
         stage.setTitle("Jogo da Forca");
         stage.setScene(scene);
         stage.show();
