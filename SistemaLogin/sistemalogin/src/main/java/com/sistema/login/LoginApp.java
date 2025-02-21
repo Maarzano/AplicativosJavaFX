ee7eb6a9c450eadf3f31b8c976b7661002525725
Marzano <arthuramarzano10@gmail.com>
2025-02-21 13:17:17 -0300

    tinha resolvido porra nenhuma, agora eu resolvi (tudo funcionando no MEU pc)

--- SistemaLogin/sistemalogin/src/main/java/com/sistema/login/LoginApp.java
+++ SistemaLogin/sistemalogin/src/main/java/com/sistema/login/LoginApp.java
@@ -10,13 +10,6 @@ import javafx.stage.Stage;
 public class LoginApp extends Application {
 
     @Override
-<<<<<<< HEAD
-    public void start(@SuppressWarnings("exports") Stage primaryStage) throws Exception {
-       
-        Parent root = FXMLLoader.load(getClass().getResource("Tela.fxml"));
-        
-        Scene scene = new Scene(root, 300, 200);
-=======
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(LoginApp.class.getResource("/com/sistema/login/Tela.fxml"));
         Image iconImage = new Image(getClass().getResource("/com/sistema/login/Imagens/17905775.gif").toExternalForm());
@@ -24,7 +17,6 @@ public class LoginApp extends Application {
         Scene scene = new Scene(root, 260, 245);
         scene.getStylesheets().add(getClass().getResource("/com/sistema/login/styles.css").toExternalForm());
 
->>>>>>> 887ffb5f783e1b6986c607ce1965b00a0b3943d9
         primaryStage.setTitle("Tela de Login");
         primaryStage.setScene(scene);
         primaryStage.show();
